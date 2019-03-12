4f9eb0286ca08833294828847dd272b06627b90b
DevVorld <mpandav1998@gmail.com>
2019-03-12 23:03:02 +0530

    readme added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import Image from './LazyImage';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Image />, document.getElementById('root'));
