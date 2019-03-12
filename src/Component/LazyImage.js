19ea86c5b288c35c355886463c65b5674942998c
DevVorld <mpandav1998@gmail.com>
2019-03-12 22:34:38 +0530

    lazyload

---
+++ src/Component/LazyImage.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class Image extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { intersected: false };
+    this.observer = null;
+  }
+
+  componentDidMount() {
+    this.observer = new IntersectionObserver(entries => {
+      const image = entries[0];
+      if (image.isIntersecting) {
+        this.setState({ intersected: true });
+        this.observer.disconnect();
+      }
+    });
+
+    this.observer.observe(this.imgTag);
+  }
+
+  componentWillUnmount() {
+    this.observer.disconnect();
+  }
+
+  render() {
+    const { src, alt, ...props } = this.props;
+
+    return (
+      <img
+        src={this.state.intersected ? src : ''}
+        alt={alt || ''}
+        ref={elem => (this.imgTag = elem)}
+        {...props}
+      />
+    );
+  }
+}
